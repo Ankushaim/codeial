452774f69ad2ac9198d133db00f67d81916e3752
Ankushaim <ankushaim@hotmail.com>
2021-04-18 18:45:43 +0530

    Connection done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const db = require('./config/mongoose');
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+
+
 app.use(express.static('./assets'));
 
-app.use(expressLayouts);
+app.use(expressLayouts); 
 
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
