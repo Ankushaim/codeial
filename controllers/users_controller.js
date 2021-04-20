746ded4589a48f5aeeece7dfd113935c6e21450e
Ankushaim <ankushaim@hotmail.com>
2021-04-20 14:38:21 +0530

    Created and rendered SignIn and SignUp pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,3 +6,26 @@ module.exports.profile = function (req, res) {
 
     );
 }
+
+//rendering signup page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Codial | SignUp"
+    })
+}
+
+//rendering signin page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: 'Codial | SignIn'
+    })
+}
+
+
+module.exports.create = function(req, res) {
+    //TODO later
+}  
+
+module.exports.createSession = function(req, res) {
+    //TODO later
+}  
