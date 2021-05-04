ba1ed2500f5567567a67757c860dc305c6abb8e6
Ankush Agrawal <ankushaim@hotmail.com>
2021-05-05 00:52:23 +0530

    posts are now showing on home page. Fome is now only visible to signed uses with the help of locals.uses

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,25 @@
+const Post = require('../models/post');
 
 module.exports.home = function(req, res) {
-    console.log(req.cookies)// accessing cookies
+
+    // console.log(req.cookies)// accessing cookies
     // res.cookie('user_id', 25);
-    return res.render('home',{
-        title: "Home",
+
+    //Helpful to find the post but not user we have mongoose populate function for this which we will use
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts,
+    //     })
+    // }); 
+
+    // to populate the user fof each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts,
+        })
     });
+
+
 };
